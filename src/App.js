018d114d47a9edb07aea068ddba42eb6f4a7a591
elijah doyle <elijah@elijahdoyle.com>
2023-03-23 21:43:10 -0400

    initial commit setting up react stuff

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react'
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        Hello World
+      </div>
+    );
+  }
+}
+
+export default App;
